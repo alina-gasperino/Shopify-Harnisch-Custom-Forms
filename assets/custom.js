88da774a7fbd6aa7110817615c3bae4c35226d7a
alina-gasperino <alinacreatesthings@gmail.com>
2024-11-28 18:03:39 -0500

    popup, mega menu, and responsive

--- assets/custom.js
+++ assets/custom.js
@@ -7,4 +7,35 @@ $(document).ready(function () {
         $(this).closest(".hulk_form").find(".modal_overlay").hide();
         $(this).closest(".hulk_form").find(".form_modal").hide();
     })
+
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+           var date = new Date();
+           date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+           expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+           var cookie = cookies[i].trim();
+           if (cookie.indexOf(nameEQ) === 0) {
+              return cookie.substring(nameEQ.length, cookie.length);
+           }
+        }
+        return null;
+    }
+
+    if (!getCookie('firstTimeVisitorPopup')) {
+        setCookie('firstTimeVisitorPopup', 'true', 30);
+        console.log("new visitor")
+    }
+    else {
+        console.log("recurring visitor")
+        $("#sca-p-popup-main-popup-container").hide()
+    }
 });
